5873447f7d56ffc24954af414aeb693b96c294ec
Wanda <wandawebworld@gmail.com>
2023-10-09 23:06:35 +0200

    add video helper and fizznodes to installed nodes

--- install_nodes/win_install_animate_diff.bat
+++ install_nodes/win_install_animate_diff.bat
@@ -1,7 +1,10 @@
 pushd %~dp0
 ..\dlbackend\comfy\python_embeded\python.exe -m pip install wget
-..\dlbackend\comfy\python_embeded\python.exe install_animate_diff.py
+START /W ..\dlbackend\comfy\python_embeded\python.exe install_animate_diff.py
+ECHO Back from ..\dlbackend\comfy\python_embeded\python.exe
 ..\dlbackend\comfy\python_embeded\python.exe -m pip install -r ..\dlbackend\comfy\ComfyUI\custom_nodes\comfyui_controlnet_aux\requirements.txt
-..\dlbackend\comfy\python_embeded\python.exe -m pip install -r ..\dlbackend\comfy\ComfyUI\custom_nodes\comfyui-animatediff\requirements.txt
+..\dlbackend\comfy\python_embeded\python.exe -m pip install -r ..\dlbackend\comfy\ComfyUI\custom_nodes\ComfyUI-VideoHelperSuite\requirements.txt
+..\dlbackend\comfy\python_embeded\python.exe -m pip install -r ..\dlbackend\comfy\ComfyUI\custom_nodes\ComfyUI_FizzNodes\requirements.txt
+..\dlbackend\comfy\python_embeded\python.exe -m pip install -r ..\dlbackend\comfy\ComfyUI\custom_nodes\ComfyUI-Advanced-ControlNet\requirements.txt
 popd
 pause
